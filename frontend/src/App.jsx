5f534a1910a943461cf6361976cd09d878fe0c22
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-11-14 13:41:10 +0000

    code successfully moved across

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react';
+import {BrowserRouter as Router } from "react-router-dom"
 import './App.css'
+import { useState } from "react"
+import Navbar from './features/navbar/Navbar.jsx'
+import AppRoutes from './features/appRoutes/AppRoutes.jsx'
+import Footer from "./features/footer/Footer.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currUser, setCurrUser]=useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="app">
+        <Navbar currUser={currUser} setCurrUser={setCurrUser}/>
+        <div className="app-body">
+          <AppRoutes currUser={currUser} setCurrUser={setCurrUser} />
+        </div>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
